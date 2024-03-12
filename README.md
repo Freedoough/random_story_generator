aa8bcb31548c495ebd8d95cee8f6300e60b956f4
Freedoough <justinfreedo@gmail.com>
2024-03-12 02:26:02 -0400

    Initial commit. Added functions and basic intro

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# new_project
+A project that generates a text based interactive story. Complete with user input, file handling, and random events to enhance replayability.
---
+++ main_.py
@@ -0,0 +1,19 @@
+import time
+import random
+
+def main():
+    
+    print("Hello!")
+    time_()
+    #print_("Welcome to this program!\n")
+
+    user_name = input("Please enter your name: ").capitalize()
+
+def intro():
+    print
+
+def time_():
+    time.sleep(3)
+def print_(text):
+    print
+main()
