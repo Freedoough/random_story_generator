29a3fef8b5a3af38f69d49448d61ffd1898fd618
Freedoough <justinfreedo@gmail.com>
2024-03-17 02:20:14 -0400

    added if statements to main for different functions

--- main_.py
+++ main_.py
@@ -14,16 +14,34 @@ def intro():
     print("Welcome to this awesome program!")
     time.sleep(2)
     user_name = input("Please enter your name: ").capitalize()
-    
+    #figure out time and print functions and pass in data to them
     print("Hello " + user_name + "!")
-
+    
     main()
 
 def main():
     #os.read("story_content_1.py", 'r')
     #figure out syntax for file reading
     time.sleep(2)
-    print("test")
+    
+    choice = input("\nTo start, pick a number from 1 to 5: ")
+
+    if choice == "1":
+        print("function 1")
+    elif choice == "2":
+        print("function 2")
+    elif choice == "3":
+        print("function 3")
+    elif choice == "4":
+        print("function 4")
+    elif choice == "5":
+        print("function 5")
+    else:
+        print("INVALID")
+        time.sleep(2)
+        print("Try again...")
+        time.sleep(1)
+        main()
 
 
 intro()
