1de34c56c0679d7431e0d6908da40d3f5f76a92c
Freedoough <justinfreedo@gmail.com>
2024-03-18 02:59:28 -0400

    Updated intro function in main.py

--- main_.py
+++ main_.py
@@ -15,8 +15,10 @@ import functions
 
 def intro():
 
-    print("Welcome to this awesome program!")
-    time.sleep(2)
+    functions.time_sleep(0, "Welcome to this awesome program!")
+    functions.time_sleep(2, "This program generates a random story for your enjoyment.")
+    functions.time_sleep(3, "Enjoy and have fun!")
+
     user_name = input("Please enter your name: ").capitalize()
     print("Hello " + user_name + "!")
     
