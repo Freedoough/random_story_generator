1239ce4eab076ea7251fae4ebeddb6c75ddf57b6
Freedoough <justinfreedo@gmail.com>
2024-04-03 02:43:31 -0400

    added functions.loading to story content 2-5

--- story_content_4.py
+++ story_content_4.py
@@ -5,13 +5,8 @@ import random
 import time
 
 def main():
-    i = 0
-
-    while i < 5:
-
-        functions.time_sleep(.75, "LOADING...\n")
-
-        i += 1
+    
+    functions.loading()
 
 
     functions.time_sleep(2, "You randomly appear in a space station...")
